6f21ce06be42f5fc485c8da14041c7a4a782eae9
Irem <irem.arslan@gmx.at>
2025-10-08 17:41:21 +0200

    Creates model layer and Document.cs

---
+++ Paperless.Models/Document.cs
@@ -0,0 +1,9 @@
+﻿namespace Paperless.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
