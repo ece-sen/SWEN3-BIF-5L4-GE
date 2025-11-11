8d956c75225cf0325b0606eced96589742e66f7f
Irem <irem.arslan@gmx.at>
2025-11-11 15:44:16 +0100

    Creates logfile

--- Paperless.REST/Program.cs
+++ Paperless.REST/Program.cs
@@ -1,57 +1,75 @@
 using Microsoft.EntityFrameworkCore;
 using Paperless.DAL;
-using Paperless.Services.Mappings;
 using Paperless.Services;
+using Paperless.Services.Mappings;
+using Serilog;
+using Serilog.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/paperless.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DMSDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DMSDb")));
-
-builder.Services.AddScoped<IDMSDbContext>(provider => provider.GetRequiredService<DMSDbContext>());
-builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
-builder.Services.AddScoped<IDocumentService, DocumentService>();
-builder.Services.AddAutoMapper(typeof(DocumentProfile).Assembly);
-
-
-builder.Services.AddCors(options =>
+try
 {
-    options.AddPolicy("AllowFrontend", policy =>
+    Log.Information("Starting Paperless REST API...");
+
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddDbContext<DMSDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DMSDb")));
+
+    builder.Services.AddScoped<IDMSDbContext>(provider => provider.GetRequiredService<DMSDbContext>());
+    builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+    builder.Services.AddScoped<IDocumentService, DocumentService>();
+    builder.Services.AddAutoMapper(typeof(DocumentProfile).Assembly);
+
+    builder.Services.AddCors(options =>
     {
-        policy
-            .WithOrigins(
-                "http://localhost:8080", "http://localhost:5173", "http://localhost:5174") 
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+        options.AddPolicy("AllowFrontend", policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:8080", "http://localhost:5173", "http://localhost:5174")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
     });
-});
 
+    var app = builder.Build();
 
-var app = builder.Build();
+    app.UseRouting();
+    app.UseCors("AllowFrontend");
 
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseRouting();
+    app.UseAuthorization();
+    app.MapControllers();
 
-app.UseCors("AllowFrontend");
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<DMSDbContext>();
+        db.Database.Migrate();
+        Log.Information("Database migration applied successfully");
+    }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.Run();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-    var db = scope.ServiceProvider.GetRequiredService<DMSDbContext>();
-    db.Database.Migrate();
+    Log.Fatal(ex, "Application startup failed");
 }
-
-app.Run();
+finally
+{
+    Log.Information("Shutting down logger...");
+    Log.CloseAndFlush();
+}
