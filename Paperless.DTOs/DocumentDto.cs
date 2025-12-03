cb63c34ac194267ab4d48ee3627407ff6adeaaed
HP_ece\Ece <if23b062@technikum-wien.at>
2025-12-03 16:58:51 +0100

    fix: dto and controller upload function is fixed - swagger does not crash anymore

--- Paperless.DTOs/DocumentDto.cs
+++ Paperless.DTOs/DocumentDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Paperless.DTOs;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Paperless.DTOs;
 
 public class DocumentDto
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
+    public IFormFile? File { get; set; }
+
 }
