f1a8cd4e5a2786fc8579eb4bd09a65135360ef63
Irem <irem.arslan@gmx.at>
2025-12-10 15:55:40 +0100

    WIP

---
+++ Paperless.Models/OcrCompletedMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paperless.Models
+{
+    public class OcrCompletedMessage
+    {
+        public int DocumentId { get; set; }
+        public string Text { get; set; } = "";
+    }
+}
+
