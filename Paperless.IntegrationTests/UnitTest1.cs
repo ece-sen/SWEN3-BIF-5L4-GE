9a05c7db332edd80f9f404106523c60f6778e172
Irem <irem.arslan@gmx.at>
2026-01-02 17:45:42 +0100

    Setup integrations tests

---
+++ Paperless.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Paperless.IntergrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
